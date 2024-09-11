90b450cfc481a030e0e150a521a09a7fe3fac407
Polina-VA <polinavakhaeva3@gmail.com>
2024-09-11 15:51:58 +0300

    created Auth servises and routes

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -2,15 +2,15 @@ const router = require("express").Router();
 const errorRouter = require('./error.routes');
 const tokensRouter = require('./tokens.routes');
 const authRouter = require('./auth.routes');
-const propertiesRouter = require('./properties.routes');
-const categoriesRouter = require('./categories.routes');
-const favoritesRouter = require('./favorites.routes');
+// const propertiesRouter = require('./properties.routes');
+// const categoriesRouter = require('./categories.routes');
+// const favoritesRouter = require('./favorites.routes');
 
 router.use('/tokens', tokensRouter);
 router.use('/auth', authRouter);
-router.use('/properties', propertiesRouter);
-router.use('/categories', categoriesRouter)
-router.use('/favorites', favoritesRouter)
+// router.use('/properties', propertiesRouter);
+// router.use('/categories', categoriesRouter)
+// router.use('/favorites', favoritesRouter)
 
 
 
