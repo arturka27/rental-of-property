91c5965a1be34e28dd430c8bb8246a8c06723802
shestakv <shevatriker@gmail.com>
2024-09-13 09:43:20 +0300

    fix: Properties card

--- client/src/pages/property/PropertyCard.jsx
+++ client/src/pages/property/PropertyCard.jsx
@@ -7,7 +7,7 @@ import ModalWindow from "../../shared/ui/ModalWindow";
 
 function PropertyCard() {
   const [active, setActive] = useState(false);
-  const [property, setProperty] = useState({});
+  const [property, setProperty] = useState(undefined);
   const { user, setProperties, categories } = useContext(AppContext);
   const { propertyId } = useParams();
 
@@ -38,56 +38,63 @@ function PropertyCard() {
 
   useEffect(() => {
     onHandleGetProperty();
-  }, []);
+  }, [active]);
 
   const isActive = () => {
     setActive((prev) => !prev);
   };
   return (
-    <div className="property">
-      <h3 className="property-title">{property.title}</h3>
-      <div>
-        <div>
-          <img
-            className="property-photo"
-            src={property.photo}
-            alt="property photo"
-            width={"400px"}
-          />
+    <>
+      {property && (
+        <div className="property">
+          <h3 className="property-title">{property.title}</h3>
+          <div>
+            <div>
+              <img
+                className="property-photo"
+                src={property.photo}
+                alt="property photo"
+                width={"400px"}
+              />
+            </div>
+            <p className="property-info">Адрес: {property.address}</p>
+            <p className="property-info">Описание: {property.description}</p>
+            <p className="property-info">
+              Стоимость в месяц: {property.price}₽
+            </p>
+          </div>
+          <div className="buttons-edit">
+            {user && user.isAdmin && (
+              <>
+                <button onClick={onHandleDelete} className="property-button">
+                  Удалить объявление
+                </button>
+                <button onClick={isActive} className="property-button">
+                  Обновить объявление
+                </button>
+              </>
+            )}
+            <button
+              onClick={() => {
+                navigate(-1);
+              }}
+              className="property-button"
+            >
+              Назад
+            </button>
+
+            <ModalWindow active={active} setActive={setActive}>
+              <PropertyFormUpdate
+                categories={categories}
+                property={property}
+                setProperties={setProperties}
+                setActive={setActive}
+              />
+            </ModalWindow>
+          </div>
         </div>
-        <p className="property-info">Адрес: {property.address}</p>
-        <p className="property-info">Описание: {property.description}</p>
-        <p className="property-info">Стоимость в месяц: {property.price}₽</p>
-      </div>
-      <div className="buttons-edit">
-        {user && user.isAdmin && (
-          <>
-            <button onClick={onHandleDelete} className="property-button">
-              Удалить объявление
-            </button>
-            <button onClick={isActive} className="property-button">
-              Обновить объявление
-            </button>
-          </>
-        )}
-        <button
-          onClick={() => {
-            navigate(-1);
-          }}
-          className="property-button"
-        >
-          Назад
-        </button>
-        <ModalWindow active={active} setActive={setActive}>
-          <PropertyFormUpdate
-            categories={categories}
-            property={property}
-            setProperties={setProperties}
-            setActive={setActive}
-          />
-        </ModalWindow>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
